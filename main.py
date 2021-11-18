27a0a69f7ec50055a52a5a93aca31dbeb948d2cb
Septatrix <24257556+Septatrix@users.noreply.github.com>
2021-11-18 19:09:55 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+
+import dotenv
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+dotenv.load_dotenv()
+
+FORWARD_FROM_CHATID = int(os.environ["FORWARD_FROM_CHATID"])
+FORWARD_TO_CHATID = int(os.environ["FORWARD_TO_CHATID"])
+
+app = Client("PollForwardingBot", bot_token=os.environ["BOT_TOKEN"])
+
+
+@app.on_message(filters.poll & filters.chat(FORWARD_FROM_CHATID))
+async def forward_poll(client, message: Message):
+    forwarded = await message.forward(FORWARD_TO_CHATID)
+    forwarded.reply_text(message.link, quote=True, disable_notification=True)
+
+
+@app.on_message(
+    (filters.new_chat_members | filters.left_chat_member)
+    & filters.chat(FORWARD_TO_CHATID)
+)
+async def delete_member_change_notification(client, message: Message):
+    return await message.delete()
+
+
+if __name__ == "__main__":
+    app.run()
